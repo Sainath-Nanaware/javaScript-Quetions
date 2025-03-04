fd35a5d7c86833337796d8578e71bc67eb2cb481
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-03-04 18:48:42 +0530

    complete pattern 3

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main(){
 		  7 8 9 10		
 	*/
 
+/*
 package main
 
 import(
@@ -60,6 +61,36 @@ func main(){
 			// fmt.Print(count)
 			fmt.Print(" ",count);
 			count++;
+		} 
+		fmt.Println()
+	}
+}
+*/
+
+//Patern 3:
+	/*
+			 1
+		    2 2
+		   3 3 3
+		  4 4 4 4		
+	*/
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	var i int=0
+	var j int=0
+	for i=1;i<=4;i++{
+		for j=4;j>=i;j--{
+			fmt.Print(" ")
+		}
+		for j=1;j<=i;j++{
+			fmt.Print(" ",i);
 		} 
 		fmt.Println()
 	}
