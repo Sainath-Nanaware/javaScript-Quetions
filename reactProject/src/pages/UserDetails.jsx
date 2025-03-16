c77898e32a944ed9beb33ed5a4fae4cfd2fc49d6
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-03-16 23:23:29 +0530

    edit registration form

--- reactProject/src/pages/UserDetails.jsx
+++ reactProject/src/pages/UserDetails.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function UserDetails() {
   return (
-    <div>
-      <h1>User Info</h1>
+    <div className='pb h-[100vh] flex flex-col justify-center items-center gap-[20px]'>
+        <h1 className='font-semibold text-3xl'>User Registration</h1>
+      <div className='pb flex flex-col justify-center items-start gap-[10px] p-[20px]'>
+        <input type="text" name='FullName' placeholder='Enter Full Name:' className='border-b-orange-50 p-[6px] w-[30vw]' />
+        <input type="email" name='Email' placeholder='Enter email:' className='pb p-[6px] w-[30vw]' />
+        <input type="text" name='Address' placeholder='Enter address:' className='pb p-[6px] w-[30vw]' />
+        <input type="text" name='Phone no' placeholder='Enter phone number:' className='pb p-[6px] w-[30vw]' />
+
+      </div>
     </div>
   )
 }
