6143c16cc003c3eaade74d4dd302a6158de40db3
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-03-21 23:15:47 +0530

    add styling

--- reactProject/src/components/AddItems.jsx
+++ reactProject/src/components/AddItems.jsx
@@ -6,20 +6,28 @@ function AddItems() {
     const inputRef=useRef()
     function handleAddEvent(){
         SetSkills((prev)=>[...prev,inputRef.current.value])
-        // inputRef.current.value='' 
-         setInputText('N/A')
+        setInputText('N/A')
+    }
+    const clear=()=>{
+        SetSkills([])
+        inputRef.current.value='' 
     }
   return (
-    <div className='pb h-[100vh] flex flex-col justify-center items-center gap-[20px]'>
-        <div className='pb h-[20vh] w-[50vw] flex justify-center items-center'>
+    <div className='pb h-[100vh] flex flex-col justify-center items-center gap-[12px]'>
+        <h1 className='text-4xl text-gray-600 underline'>Your Skill Stack!</h1>
+        <div className='h-[20vh] w-[50vw] flex justify-center items-center'>
             <p className=' w-[40vw] p-[12px]'>{inputText}</p>
         </div>
         <div className='h-[20vh] w-[50vw] flex flex-col justify-center items-center gap-[10px]'>
             <input type="text" placeholder='Enter Your Skills' onChange={(e)=>{setInputText(e.target.value)}} ref={inputRef} className='pb  rounded-2xl w-[40vw] p-[10px]' />
+            <div className='flex justify-center gap-4'>
             <button className='w-[8vw] p-[5px] rounded-3xl bg-green-500' onClick={()=>{handleAddEvent()}}>Add</button>
+            <button className='w-[8vw] p-[5px] rounded-3xl bg-red-500' onClick={()=>{clear()}}>Clear</button>
+            </div>
+
         </div>
-        <div className='pb h-[20vh] w-[50vw] flex justify-center items-center'>
-            <p className=' w-[40vw] p-[12px]'>Your Skill List:{skills.map((element,index)=>{
+        <div className='overflow-auto border-2 border-amber-200 rounded-3xl   h-[30vh] w-[50vw] flex justify-center items-center'>
+            <p className='  w-[40vw] p-[10px]'>Your Skill List:{skills.map((element,index)=>{
                         return <li key={index}>{element}</li>
                 })}</p>
         </div>
